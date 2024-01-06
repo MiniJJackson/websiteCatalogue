0474ec6980283d4624a656650fe23093104de3f5
Jenna Van De Vyver <r0889235@student.thomasmore.be>
2024-01-06 22:52:04 +0100

    ADDED API file - lets get testing the app :)

---
+++ config/element.api.php
@@ -0,0 +1,45 @@
+<?php
+
+use craft\elements\Entry;
+use craft\elements\Asset;
+use craft\helpers\UrlHelper;
+
+return [
+    'endpoints' => [
+        '/api/clothing' => function() {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['section' => 'clothing'],
+                'cache' => false,
+                'serializer' => 'jsonFeed',
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'id' => $entry->id,
+                        'title' => $entry->title,
+                        'brand' => $entry->brand,
+                        'intro' => $entry->introText,
+                        'bannerImg' => str_replace("https", "http", $entry->bannerImage->one()->getUrl('bannerImage')),
+                        'thumbImg' => str_replace("https", "http", $entry->bannerImage->one()->getUrl('thumbnailImage')),
+                    ];
+                },
+            ];
+        },
+        '/api/clothing/<entryId:\d+>' => function($entryId) {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['id' => $entryId],
+                'one' => true,
+                'cache' => false,
+                'serializer' => 'jsonFeed',
+                'transformer' => function(Entry $entry) {
+                  return [
+                      'id' => $entry->id,
+                      'title' => $entry->title,
+                      'fullText' => $entry->fullText,
+                      'bannerImg' => str_replace("https", "http", $entry->bannerImage->one()->getUrl('bannerImage')),
+                  ];
+              },
+            ];
+        },
+    ]
+];
